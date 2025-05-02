cebd8ec3376577afb37be54865c6506ddd1f09c7
Wabi <119341100+Wahab69420@users.noreply.github.com>
2025-05-02 14:51:27 +0500

    Time: 19 ms (16.39%), Space: 18.6 MB (11.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
